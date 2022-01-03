fa788489692bccce81432f46a1b79c764f83a16f
Omar Faruqe <faruqe.cse@gmail.com>
2022-01-03 16:41:02 +0600

    Parameterized Lambda

--- src/com/omarfaruqe/LambdaExpression.java
+++ src/com/omarfaruqe/LambdaExpression.java
@@ -1,31 +1,31 @@
 package com.omarfaruqe;
 @FunctionalInterface
 interface Cab{
-    void bookCab();
+    void bookCab(String source, String destination);
 }
 
-class UberX implements Cab {
-    @Override
-    public void bookCab() {
-        System.out.println("UberX Booked !! Arriving Soon!!");
-    }
-}
+//class UberX implements Cab {
+//    @Override
+//    public void bookCab() {
+//        System.out.println("UberX Booked !! Arriving Soon!!");
+//    }
+//}
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        Cab cab = new UberX();
-        cab.bookCab();
+//        Cab cab = new UberX();
+//        cab.bookCab();
+//
+//        Cab cabWithAnonymous = new Cab(){
+//            @Override
+//            public void bookCab() {
+//                System.out.println("UberX Booked !! Arriving Soon!!");
+//            }
+//        };
+//        cabWithAnonymous.bookCab();
 
-        Cab cabWithAnonymous = new Cab(){
-            @Override
-            public void bookCab() {
-                System.out.println("UberX Booked !! Arriving Soon!!");
-            }
-        };
-        cabWithAnonymous.bookCab();
-
-        Cab cabWithLambda = () ->
-            System.out.println("UberX Booked !! Arriving Soon!!");
-        cabWithLambda.bookCab();
+        Cab cabWithLambda = (source, destination) ->
+            System.out.println("UberX Booked from " + source + " To " + destination + ", Arriving Soon!!");
+        cabWithLambda.bookCab("Dhaka", "Rajshahi");
     }
 }
