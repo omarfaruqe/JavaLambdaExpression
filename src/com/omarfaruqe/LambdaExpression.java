58f9c590d39dd330e9888a15ed4a36433a7b2339
Omar Faruqe <faruqe.cse@gmail.com>
2022-01-03 15:28:06 +0600

    Initial Project with Funcational Interface

---
+++ src/com/omarfaruqe/LambdaExpression.java
@@ -0,0 +1,20 @@
+package com.omarfaruqe;
+@FunctionalInterface
+interface Cab{
+    void bookCab();
+}
+
+class UberX implements Cab {
+    @Override
+    public void bookCab() {
+        System.out.println("UberX Booked !! Arriving Soon!!");
+    }
+}
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        Cab cab = new UberX();
+        cab.bookCab();
+    }
+
+}
