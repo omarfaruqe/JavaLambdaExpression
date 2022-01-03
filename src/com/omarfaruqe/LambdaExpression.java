997f86a2fa09eeccf1c34e37df15bbbcd85cf94c
Omar Faruqe <faruqe.cse@gmail.com>
2022-01-03 16:23:47 +0600

    Using Anonymous Class

--- src/com/omarfaruqe/LambdaExpression.java
+++ src/com/omarfaruqe/LambdaExpression.java
@@ -15,6 +15,13 @@ public class LambdaExpression {
     public static void main(String[] args) {
         Cab cab = new UberX();
         cab.bookCab();
-    }
 
+        Cab cabWithAnonymous = new Cab(){
+            @Override
+            public void bookCab() {
+                System.out.println("UberX Booked !! Arriving Soon!!");
+            }
+        };
+        cabWithAnonymous.bookCab();
+    }
 }
